4f3d9df6fe6ba298485240473297f4c56b863743
Alexey Prikhodko <al.prykhodko94@gmail.com>
2016-11-15 20:57:31 +0300

    Ещё небольшие изменения

--- AIPPS_Lab1/AIPPS_Lab1/Iterator.cpp
+++ AIPPS_Lab1/AIPPS_Lab1/Iterator.cpp
@@ -63,8 +63,8 @@ int main()
 	for (i = 1; i < 5; i++)
 		s1.push(i);
 	Stack s2(s1), s3(s1), s4(s1), s5(s1);
-	s5.pop();
 	s3.pop();
+	s5.pop();
 	s4.push(2);
 	s5.push(9);
 	std::cout << "1 == 2 is " << (s1 == s2) << std::endl;
