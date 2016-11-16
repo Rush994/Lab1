472092acaa042e545c6e0835bde792b682ee29d9
Alexey Prikhodko <al.prykhodko94@gmail.com>
2016-11-16 17:37:22 +0300

    Комменты в коде Итератора

--- AIPPS_Lab1/AIPPS_Lab1/Iterator.cpp
+++ AIPPS_Lab1/AIPPS_Lab1/Iterator.cpp
@@ -58,19 +58,21 @@ bool operator == (const Stack &l, const Stack &r)
 
 int main()
 {
-	Stack s1;
+	Stack s1; // Созддаём объект стека с начальной глубиной -1
 	int i;
-	for (i = 1; i < 5; i++)
+	for (i = 1; i < 5; i++) // Заполняе его 4 значениями
 		s1.push(i);
+	// После цикла sp=3 значения 
+	// Создаём ещё 4 объекта стека
 	Stack s2(s1), s3(s1), s4(s1), s5(s1);
-	s3.pop();
-	s5.pop();
-	s4.push(2);
-	s5.push(9);
-	std::cout << "1 == 2 is " << (s1 == s2) << std::endl;
-	std::cout << "1 == 3 is " << (s1 == s3) << std::endl;
-	std::cout << "1 == 4 is " << (s1 == s4) << std::endl;
-	std::cout << "1 == 5 is " << (s1 == s5) << std::endl;
+	s3.pop();	// -1 из s3(sp=2 значения)
+	s5.pop();	// -1 из s5(sp=2 значения)
+	s4.push(2);	// +1 в s4(sp=4 значений)
+	s5.push(9);	// +1 в s5(sp=3 значений)
+	std::cout << "1 == 2 is " << (s1 == s2) << std::endl; // 1
+	std::cout << "1 == 3 is " << (s1 == s3) << std::endl; // 0
+	std::cout << "1 == 4 is " << (s1 == s4) << std::endl; // 0
+	std::cout << "1 == 5 is " << (s1 == s5) << std::endl; // 0
 	std::cin.get();
 	return 0;
 }
